232e651a7b5e9a5d95ac8a63257a00ad680e5252
Jasper <jave.ong4@yahoo.com>
2017-02-19 17:46:52 -0500

    Updates

--- Learn-Redux-Starter-Files/learn-redux/client/reduxstagram.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-// import css
-import css from './styles/style.styl';
-
-import Main from './components/Main'
-
-render(<Main />, document.getElementById('root'));
